de96765481154720585d97e25407648ff0243273
Nemesisrf <genesiisrojjass@gmail.com>
2021-09-21 19:13:50 -0500

    primer commit

---
+++ departamentos.js
@@ -0,0 +1,99 @@
+const capitales = () => {
+  const department = prompt("Por favor introduzca el departamneto");
+
+  switch (department) {
+    case "Amazonas":
+      alert("Su capital es Leticia");
+      break;
+    case "Arauca":
+      alert("Su capital es Arauca");
+      break;
+    case "Atlántico":
+      alert("Su capital es Barranquilla");
+      break;
+    case "Bolívar":
+      alert("Su capital es Cartagena de Indias");
+      break;
+    case "Boyacá":
+      alert("Su capital es Tunja");
+      break;
+    case "Caldas":
+      alert("Su capital es Manizales");
+      break;
+    case "Caquetá":
+      alert("Su capital es Florencia");
+      break;
+    case "Casanare":
+      alert("Su capital es Yopal");
+      break;
+    case "Cauca":
+      alert("Su capital es Popayán");
+      break;
+    case "Cesar":
+      alert("Su capital es Valledupar");
+      break;
+    case "Chocó":
+      alert("Su capital es Quibdó");
+      break;
+    case "Córdoba":
+      alert("Su capital es Montería");
+      break;
+    case "Cundinamarca":
+      alert("Su capital es Bogotá");
+      break;
+    case "Guainía":
+      alert("Su capital es Inírida");
+      break;
+    case "Guaviare":
+      alert("Su capital es San José del Guaviare");
+      break;
+    case "Huila":
+      alert("Su capital es Neiva");
+      break;
+    case "La Guajira":
+      alert("Su capital es Riohacha");
+      break;
+    case "Magdalena":
+      alert("Su capital es Santa Marta");
+      break;
+    case "Meta":
+      alert("Su capital es Villavicencio");
+      break;
+    case "Nariño":
+      alert("Su capital es San Juan de Pasto");
+      break;
+    case "Norte d: Santander":
+      alert("Su capital es San José de Cúcuta");
+      break;
+    case "Putumayo":
+      alert("Su capital es Mocoa");
+      break;
+    case "Quindío":
+      alert("Su capital es Armenia");
+      break;
+    case "Risaralda":
+      alert("Su capital es Pereira");
+      break;
+    case "San André: y Providencia":
+      alert("Su capital es San Andrés");
+      break;
+    case "Santander":
+      alert("Su capital es Bucaramanga");
+      break;
+    case "Sucre":
+      alert("Su capital es Sincelejo");
+      break;
+    case "Tolima":
+      alert("Su capital es Ibagué");
+      break;
+    case "Valle de: Cauca":
+      alert("Su capital es Cali");
+      break;
+    case "Vaupés":
+      alert("Su capital es Mitú");
+      break;
+    case "Vichada":
+      alert("Su capital es Puerto Carreñon");
+      break;
+  }
+};
